4ba5ef8da111ab41b77e46e0af80b92aa0348820
Ac <406175320@qq.com>
2019-10-23 00:45:42 +0800

    字符串第二题

--- 字符串/字符串全排列.js
+++ 字符串/字符串全排列.js
@@ -1,17 +1,28 @@
-function f(arr, start ,end) {
-    if (start === end) {
+function checkchongfu(arr,_p,_q) {
+    for(let i = _p;i<_q;i++){
+        if (arr[_q] === arr[i]) {
+            console.log(1);
+            return false;
+        }
+    }
+    return true;
+}
+
+function f(arr, p ,q) {
+    if (p === q) {
             console.log(arr.join(''));
     } else {
-        for(let i = start;i <end;i++){
-            [arr[start],arr[i]] = [arr[i],arr[start]];
-             // console.log("第一："+arr.join(''));
-            f(arr,start+1,end);
-            // console.log("第二："+arr.join(''));
-            [arr[start],arr[i]] = [arr[i],arr[start]];
-             // console.log("第二："+arr.join(''));
+        for(let i = p;i <q;i++){
+            if(checkchongfu(arr,p,i)) {
+                [arr[p], arr[i]] = [arr[i], arr[p]];
+                // console.log(`i:${i}`);
+                // console.log(`p:${p}`);
+                f(arr, p + 1, q);
+                [arr[p], arr[i]] = [arr[i], arr[p]];
+            }
         }
     }
 }
 
-let arr = ['a','b','c'];
+let arr = ['a','b','c','d'];
 f(arr,0,arr.length);
